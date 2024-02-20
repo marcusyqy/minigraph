f4bc4a409e62c6624abb5054d95777af347deb33
Marcus Yong <marcusyqy@gmail.com>
2024-02-20 01:22:56 -0500

    remove some dead code.

--- minigraph/meta.hpp
+++ minigraph/meta.hpp
@@ -14,22 +14,6 @@ struct Type_List {
 };
 
 template <typename T>
-struct Element_Size : std::tuple_size<T> {
-    // static constexpr auto value = std::tuple_size_v<T>;
-};
-
-template <typename T>
-constexpr auto element_size = Element_Size<T>::value;
-
-template <size_t I, typename T>
-struct Element_Indexer {
-    using type = std::tuple_element_t<I, T>;
-};
-
-template <size_t I, typename T>
-using Element_Indexer_Type = typename Element_Indexer<I, T>::type;
-
-template <typename T>
 struct Is_Tuple_Like : std::false_type {};
 
 template <typename... Ts>
